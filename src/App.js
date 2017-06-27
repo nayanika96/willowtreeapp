b4474ac039615d675a0497a0e0f2ef9dd5ea9452
nayanika <nayanika@d-172-25-79-99.dhcp.virginia.edu>
2017-06-27 13:48:40 -0400

    home page without header

--- src/App.js
+++ src/App.js
@@ -12,19 +12,23 @@ class App extends Component {
       <div>
       <div className="App">
       </div>
-        <div>
-        <img class="image is loaded" src="//images.contentful.com/3cttzl4i3k1h/4os6mdHSNywUGQ4suqu8ko/10d81b0d0a6cc3e963cedc60215c6155/IMG_4335-267.jpg?w=731&h=881&q=80&fm=&fl=progressive&fit=fill" height="800" width="700"/>
-        <a class="header-logo-link" data-automation="header-logo" href="/" data-reactid="9">WillowTree, Inc.</a>
-        </div>
+        <div className="head">
+        
+             <div className='aboutImage'>
+            <img class="image is loaded" src="//images.contentful.com/3cttzl4i3k1h/4os6mdHSNywUGQ4suqu8ko/10d81b0d0a6cc3e963cedc60215c6155/IMG_4335-267.jpg?w=731&h=881&q=80&fm=&fl=progressive&fit=fill" height="800" width="700"/>
+            </div>
 
-         <div className="content">
-            <div className="heroheader">
+            <div className='aboutText'>
+          
+            <div className="info">
             <h1> What we do </h1>
+            <p> We build apps, responsive sites, bots—an</p>
+            <p> lives on a screen—for the world dwqkndqd</p>
+            <p>Our elite teams challenge themselves tsaxdt</p>
+            <p> and design thinking with enterprise-grade </p>
             </div>
-            <p> We build apps, responsive sites, bots—any digital product that lives on a screen—for the world’s leading companies.</p>
-            <p>Our elite teams challenge themselves to build extraordinary experiences by bridging the latest strategy
-               and design thinking with enterprise-grade software development </p>.
             </div>
+        </div>
 
           <div class="second">
 
